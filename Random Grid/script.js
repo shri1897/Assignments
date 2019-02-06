0f92be795e72ab0bdcf1fda44b8938d70f6e3ce8
Shriram <shriram1897@gmail.com>
2019-02-06 14:49:44 +0530

    Updated Random Grid/Script.js to make it readable.

--- Random Grid/script.js
+++ Random Grid/script.js
@@ -5,10 +5,11 @@ main.startGame = function (MAX_BUTTONS) {
     var i, answer, setOfValues, btn, container;
     MAX_BUTTONS = typeof MAX_BUTTONS === "number" && MAX_BUTTONS > 0 ? MAX_BUTTONS : 16;
     setOfValues = new Set(); // creating an empty set.
-    /**---------------------------------------------------------------------------------------------------- */
+
     document.getElementById("main-body").onclick = function print(event) { //onClick listener to the Body
         var clickedElement, value;
         clickedElement = event.target; //getting the reference of the clicked object
+
         if (clickedElement.getAttribute("typeName") === "random-block") { //checking if the clicked object is a button using custom attribute
             clickedElement.classList.remove("unselectedClass"); //removing all the classes
             clickedElement.classList.add("selectedClass"); //adding a class to style the selected buttons
@@ -16,20 +17,22 @@ main.startGame = function (MAX_BUTTONS) {
             if (value === answer) { //validating
                 alert("Congrats!");
                 initGame(); // Restarting the game
-            } else if (clickedElement.textContent > answer) {
+            } 
+            else if (clickedElement.textContent > answer) {
                 alert("Wrong! This is too big.");
-            } else {
+            } 
+            else {
                 alert("Wrong! i'm not that small");
             }
         }
     };
-    /**---------------------------------------------------------------------------------------------------- */
+    
     window.onkeypress = function reset(event) { // when enter is pressed it will get the input and restart the game with that many boxes.
         if (event.which === 13) {
             main.startGame(parseInt(document.getElementById("max-buttons").value));
         }
     }
-    /**---------------------------------------------------------------------------------------------------- */
+
     function initGame() { // Initializer
         container = document.getElementById("container");
         if(container) { //removing previous buttons.
@@ -38,27 +41,10 @@ main.startGame = function (MAX_BUTTONS) {
         container = document.createElement("div");
         container.id = "container";
         document.getElementById("main-body").appendChild(container);
-
-        /** OLD AND SLOW ALGORITHM TO CREATE BOXES WITH RANDOM NUMBERS:----> Omega(N*N) & O(Infinity) */
-        // for (i = 1; i <= MAX_BUTTONS; i += 1) { //getting random value for each button and adding class
-        //     btn = document.createElement("button");
-        //     btn.textContent = Math.floor((Math.random() * MAX_BUTTONS + 1));
-        //     btn.id = "box-" + i;
-        //     btn.classList.add("unselectedClass");
-        //     btn.setAttribute("typeName", "random-block"); //Testing setAttribute method
-        //     while (setOfValues.has(btn.textContent)) { //checking for repeating values
-        //         btn.textContent = Math.floor((Math.random() * MAX_BUTTONS + 1));
-        //     }
-        //     setOfValues.add(btn.textContent);
-        //     document.getElementById("container").appendChild(btn); //adding created button to div-container
-        // }
-
-        /** NEW AND FASTER ALGORITHM TO CREATE BOXES WITH RANDOM NUMBERS ---> REDUCED TO O(2N) */
         setOfValues = [];
         for (i = 1; i <= MAX_BUTTONS; i += 1) { //initializing array
             setOfValues[i] = i;
         } 
-        /**---------------------------------------------------------------------------------------------------- */
         for (i = 1; i <= MAX_BUTTONS; i += 1) { //getting random value for each button and adding class
             var temp = Math.floor((Math.random() * (MAX_BUTTONS-i+1) + i))
             btn = setOfValues[temp];
@@ -73,7 +59,6 @@ main.startGame = function (MAX_BUTTONS) {
         }
         setOfValues = []; //clearing the set for reuse.
 
-    /**---------------------------------------------------------------------------------------------------- */
         answer = "box-" + Math.floor(Math.random() * MAX_BUTTONS + 1);
         answer = parseInt(document.getElementById(answer).textContent, 10); //choosing a random value from the created boxes
     }
